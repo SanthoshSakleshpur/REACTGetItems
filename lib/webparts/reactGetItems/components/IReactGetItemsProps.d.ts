41c9829496cfd62f6e3452d92cde94430c275557
snthsh <26085932+snthsh@users.noreply.github.com>
2022-03-10 20:28:13 +0000

    fetch items and display

--- lib/webparts/reactGetItems/components/IReactGetItemsProps.d.ts
+++ lib/webparts/reactGetItems/components/IReactGetItemsProps.d.ts
@@ -2,4 +2,12 @@ export interface IReactGetItemsProps {
     description: string;
     siteurl: string;
 }
+interface Item {
+    Title: string;
+    Department: string;
+}
+export interface IReactGetItemsState {
+    items: Array<Item>;
+}
+export {};
 //# sourceMappingURL=IReactGetItemsProps.d.ts.map
--- lib/webparts/reactGetItems/components/ReactGetItems.d.ts
+++ lib/webparts/reactGetItems/components/ReactGetItems.d.ts
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import { IReactGetItemsProps } from './IReactGetItemsProps';
-export default class ReactGetItems extends React.Component<IReactGetItemsProps, {}> {
+import { IReactGetItemsProps, IReactGetItemsState } from './IReactGetItemsProps';
+export default class ReactGetItems extends React.Component<IReactGetItemsProps, IReactGetItemsState> {
     constructor(props: IReactGetItemsProps);
     componentDidMount(): void;
     render(): React.ReactElement<IReactGetItemsProps>;
